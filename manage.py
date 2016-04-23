bea10e74e9ed89cff53cb33a5eee9437a1da72a9
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-04-22 23:09:01 -0300

    Added files via upload

---
+++ manage.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+import click
+from bottle import static_file, Bottle, run, TEMPLATE_PATH, error, jinja2_view
+from beaker.middleware import SessionMiddleware
+
+from corpuslinguistics import settings
+from corpuslinguistics.routes import Routes
+
+
+TEMPLATE_PATH.insert(0, settings.TEMPLATE_PATH)
+session_opts = {
+    'session.type': 'file',
+    'session.auto': True
+}
+
+app = SessionMiddleware(Bottle(), session_opts)
+
+# Bottle Routes
+app.wrap_app.merge(Routes)
+
+@app.wrap_app.error(404)
+@jinja2_view('corpuslinguistics/views/out/upload_error.html')
+def error(error):
+    return dict(out="Essa página não existe no sistema", title="404")
+
+
+@app.wrap_app.route('/assets/<path:path>', name='assets')
+def assets(path):
+    yield static_file(path, root=settings.STATIC_PATH)
+
+
+@click.group()
+def cmds():
+    pass
+
+
+@cmds.command()
+@click.option('--port', default=os.environ.get('PORT', 8080), type=int,
+              help=u'Set application server port!')
+@click.option('--ip', default='0.0.0.0', type=str,
+              help=u'Set application server ip!')
+@click.option('--debug', default=False,
+              help=u'Set application server debug!')
+def runserver(port, ip, debug):
+    click.echo('Start server at: {}:{}'.format(ip, port))
+    run(app=app, host=ip, port=port, debug=debug, reloader=debug)
+
+
+@cmds.command()
+def test():
+    import unittest
+    loader = unittest.TestLoader()
+    tests = loader.discover('tests')
+    testRunner = unittest.runner.TextTestRunner()
+    testRunner.run(tests)
+
+
+if __name__ == "__main__":
+    cmds()
