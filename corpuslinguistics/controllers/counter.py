809a472ca57ea8e74e07f4a583e74f12b7e63956
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-04-22 23:18:29 -0300

    Create counter.py

---
+++ corpuslinguistics/controllers/counter.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from bottle import Bottle, jinja2_view, post, request, redirect
+from ..models.Auxiliar import Auxiliar, contagem
+
+# -------------- Controle das views de contagem
+cl_counter = Bottle(True)
+
+"""
+### --------------  Página do Contador   -------------- ###
+"""
+
+# -------------- Página: Contador
+@cl_counter.route('/contador')
+@jinja2_view('sistema/contador.html')
+def contador(upload=''):
+    return dict(title = 'Contador',
+                upload = upload)
+
+# -------------- Post: Contador
+@cl_counter.post('/contador')
+@jinja2_view('out/counter')
+def upload_contador():
+    auxiliar = Auxiliar()
+    pasta = auxiliar.contador
+
+    arquivo = request.files.get('arquivo')
+
+    chave = auxiliar.upload_um_arquivo(arquivo, pasta)
+
+    if chave == 0:
+        return redirect("/error_0")
+    elif chave == 1:
+        contagem(arquivo.filename)
+        saida = open("/tmp/contador/saida.txt", "r")
+        return dict(rows=saida, name="Word Counter")
+    elif chave == 2:
+        return redirect("/error_2")
