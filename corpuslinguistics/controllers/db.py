47a35a99d378bab273f3ac0866bdfe27425dd8aa
Eduardo Mendes <z4r4tu5tr4@users.noreply.github.com>
2016-04-23 23:32:11 -0300

    Update db.py
    WTFORMS

--- corpuslinguistics/controllers/db.py
+++ corpuslinguistics/controllers/db.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 from bottle import Bottle, jinja2_view, post, request
+from wtforms import StringField, PasswordField, Form, fields, SubmitField
 from ..models.base import Base
+from hashlib import sha512
 
 # -------------- Controle das views de concordancia
 cl_db = Bottle(True)
+
+"""
+### --------------  Form Cadastro -------------- ###
+"""
+class Cadastro(Form):
+    username = StringField('Username')
+    email = StringField('Email')
+    password = PasswordField('Password')
+    butt = SubmitField('OK!')
 
 """
 ### --------------  Páginas Administrativas -------------- ###
@@ -13,23 +24,28 @@ cl_db = Bottle(True)
 @cl_db.route('/cadastro')
 @jinja2_view('adm/cadastro.html')
 def cadastro():
-    return dict(title = 'Cadastro')
+    return dict(title = 'Cadastro', form = Cadastro())
 
 # -------------- Insere: Cadastro
 @cl_db.post('/cadastro')
 #@jinja2_view()
 def cadastro():
     db = Base()
-    nome = request.forms.get('nome')
-    email = request.forms.get('mail')
+    form = Cadastro(request.POST)   # ----- POST METHOD
+    name = form.username.data
+    email = form.email.data
+    password = form.password.data
+    password = sha512(password.encode()) # ----- Hashed password
+
     try:
-        assert nome != ''
+        assert name!= ''
         assert email != '' and "@" in email
-        db.inserir_dados(nome,email)
+        db.inserir_dados(name,email, password.hexdigest())
         db.commit()
         return "ok"
     except:
         return "error"
+
 """
 ### --------------  Pagina de visualização dos dados do banco   -------------- ###
 """
