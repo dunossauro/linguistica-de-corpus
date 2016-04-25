7922ddd9a68d92404a8ca71b26008c8f688c13bd
z4r4tu5tr4 <mendesxeduardo@gmail.com>
2016-04-25 15:30:31 -0300

    simple unnittests

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,13 @@
 from webtest import TestApp
 import unittest
 import manage
-
+from corpuslinguistics.models.base import Base
+from corpuslinguistics.models.Auxiliar import Auxiliar
+from os import listdir
 routes = []
 
 # ----- Test ALL PAGES
 class test_pages(unittest.TestCase):
-
     def test_routes(self):
         for route in manage.app.wrap_app.routes:
             route = str(route).split()
@@ -25,3 +26,32 @@ class test_pages(unittest.TestCase):
             self.assertEqual(resp.status_int, 200)
             self.assertEqual(resp.charset,'UTF-8')
             self.assertEqual(resp.status,'200 OK')
+
+# ----- Test DB
+class test_db(unittest.TestCase):
+    def test_insert(self):
+        base = Base()
+
+        nome = 'test'
+        email = 'test@test.com'
+        senha = 'test'
+        base.inserir_dados(nome, email, senha)
+
+        bd_return = base.login('test','test')
+
+        self.assertEqual(bd_return[0][1], 'test')
+
+    def test_select(self):
+        base = Base()
+
+        self.assertIsNot(base.busca(), [])
+
+# ----- Test Auxiliar functions
+class test_auxiliar(unittest.TestCase):
+    def test_cria_pasta(self):
+        aux = Auxiliar()
+
+        palavra = 'teste'
+        aux.cria_pasta(palavra)
+
+        self.assertIn(palavra, listdir('/tmp'))
